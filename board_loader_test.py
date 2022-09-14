54578129d9590d14333ca92a2ad444743a10e76d
hirollr <marshingjay@gmail.com>
2022-09-14 14:16:25 -0700

    board loader test

---
+++ board_loader_test.py
@@ -0,0 +1,19 @@
+import time
+import torch
+import chess
+from nn import convert_to_nn_state
+
+device = torch.device('cpu')
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+else:
+    torch.set_default_tensor_type(torch.FloatTensor)
+torch.set_default_dtype(torch.float)
+
+board = chess.Board()
+time_start = time.time()
+for i in range(1000):
+    convert_to_nn_state(board)
+print('took', time.time() - time_start)
+
