a1b12850f4a5117ca57aa915d2d8468192dbae88
hirollr <marshingjay@gmail.com>
2022-09-14 15:50:11 -0700

    :(

--- evaluation.py
+++ evaluation.py
@@ -132,8 +132,7 @@ class MCST_Evaluator:
             return res
         else:
             index = torch.argmax(scores)
-            val = torch.max(scores)
-            res =  (val.item(), index.item(), legal_moves[index])
+            res =  (scores[index], index.item(), legal_moves[index])
             self.choose_time += time.time() - choose_time
             return res
         
