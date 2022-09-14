ac18631bba49116ee0ef50f3292248fe8b1e3e5f
hirollr <marshingjay@gmail.com>
2022-09-14 15:11:59 -0700

    test w eval

--- board_loader_test.py
+++ board_loader_test.py
@@ -1,6 +1,7 @@
 import time
 import torch
 import chess
+from evaluation import MCST_Evaluator
 from nn import HyperionDNN, convert_to_nn_state
 
 device = torch.device('cpu')
@@ -10,10 +11,12 @@ if torch.cuda.is_available():
 else:
     torch.set_default_tensor_type(torch.FloatTensor)
 torch.set_default_dtype(torch.float)
+
 king = HyperionDNN().to(device)
+evaluator = MCST_Evaluator(king)
 board = chess.Board()
 time_start = time.time()
-for i in range(1000):
-    king(convert_to_nn_state(board).unsqueeze(0))
+for i in range(10):
+    evaluator.choose_move(board, False, False)
 print('took', time.time() - time_start)
 
