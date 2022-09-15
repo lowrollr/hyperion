3c0e741362e991ddb5688085ce4d28a717ee89f9
hirollr <marshingjay@gmail.com>
2022-09-15 14:15:59 -0700

    mp maybe

---
+++ main.py
@@ -0,0 +1,4 @@
+from train import mp_train
+
+if __name__ == '__main__':
+    mp_train()
--- train.py
+++ train.py
@@ -1,9 +1,5 @@
 # routine to continuously train and update the neur
-
-
-
-from copy import deepcopy
-from tracemalloc import start
+import torch.multiprocessing as mp
 from evaluation import MCST_Evaluator
 from nn import HyperionDNN
 from torch.optim import sgd
@@ -11,44 +7,53 @@ import chess
 import torch
 import time
 
-torch.backends.cudnn.benchmark = True
-device = torch.device('cpu')
-if torch.cuda.is_available():
-    device = torch.device("cuda:0")
-    torch.set_default_tensor_type('torch.cuda.FloatTensor')
-else:
-    torch.set_default_tensor_type(torch.FloatTensor)
-torch.set_default_dtype(torch.float)
-
-king = HyperionDNN().to(device)
-print(device)
-# king.load_state_dict(torch.load('./king_53.pth'))∂
-
-optimizer = torch.optim.Adam(king.parameters(), lr=1e-3)
-evaluator = MCST_Evaluator(king, device)
-game_count = 0
-board = chess.Board()
 
 
-while True:
-    start_time = time.time()
-    with torch.no_grad():
-        move, eval = evaluator.make_best_move(board)
-    if move is None:
-        evals = evaluator.training_evals
-        results = evaluator.training_results
-        optimizer.zero_grad()
-        loss = torch.nn.functional.l1_loss(torch.cat(evals), torch.cat(results).unsqueeze(1).unsqueeze(1))
-        loss.backward()
-        print(loss)
-        
-        board = chess.Board()
-        torch.save(king.state_dict(), f"king_{game_count}.pth")
-        game_count += 1
-        start_time
+
+def train(evaluator, optimizer):
+    evals = []
+    results = []
+    board = chess.Board()
+    while len(evals) < 10000:
+        start_time = time.time()
+        with torch.no_grad():
+            move, eval = evaluator.make_best_move(board)
+        if move is None:
+            evals.extend(evaluator.training_evals)
+            results.extend(evaluator.training_results)
+            board = chess.Board()
+        else:
+            print(move.uci(), eval, time.time() - start_time)
+            print(board)
+
+    optimizer.zero_grad()
+    loss = torch.nn.functional.l1_loss(torch.cat(evals), torch.cat(results).unsqueeze(1).unsqueeze(1))
+    loss.backward()
+    return loss.item()
+
+def mp_train():
+    torch.backends.cudnn.benchmark = True
+    device = torch.device('cpu')
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0")
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
     else:
-        print(move.uci(), eval)
-        print(board)
-        print('time', time.time() - start_time)
-        pass
-    
+        torch.set_default_tensor_type(torch.FloatTensor)
+    torch.set_default_dtype(torch.float)
+
+
+    king = HyperionDNN().to(device)
+    king.share_memory()
+    # king.load_state_dict(torch.load('./king_53.pth'))∂
+
+    optimizer = torch.optim.Adam(king.parameters(), lr=1e-3)
+    evaluator = MCST_Evaluator(king, device)
+    num_procs = mp.cpu_count() - 1
+
+    procs = []
+    for r in range(num_procs):
+        p = mp.Process(target=train, args=(evaluator, optimizer))
+        p.start()
+        procs.append(p)
+    for p in procs:
+        p.join()
