4a29f6352fcfcfb14a2d792a9a33ea1e65123632
hirollr <marshingjay@gmail.com>
2022-09-16 12:55:58 -0700

    nvm that was for cacheing?

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ if __name__ == '__main__':
     device = torch.device('cpu')
     if torch.cuda.is_available():
         device = torch.device("cuda:0")
-        torch.cuda.set_per_process_memory_fraction(0.125, device=device)
         torch.set_default_tensor_type('torch.cuda.FloatTensor')
     else:
         torch.set_default_tensor_type(torch.FloatTensor)
