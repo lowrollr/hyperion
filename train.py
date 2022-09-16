694ed1c87001209d1582117818b0382831f8710f
hirollr <marshingjay@gmail.com>
2022-09-16 12:22:12 -0700

    remove no grad

--- train.py
+++ train.py
@@ -17,8 +17,7 @@ def train(evaluator, optimizer, p_id):
     board = chess.Board()
     while len(evals) < 20:
         start_time = time.time()
-        with torch.no_grad():
-            move, eval = evaluator.make_best_move(board, 2)
+        move, eval = evaluator.make_best_move(board, 2)
         if move is None:
             evals.extend(evaluator.training_evals)
             results.extend(evaluator.training_results)
