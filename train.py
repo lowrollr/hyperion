a0ff0c499d951bd3581ec0c2cccf0b9541dc9122
hirollr <marshingjay@gmail.com>
2022-09-15 19:27:35 -0700

    dont use no grad?

--- train.py
+++ train.py
@@ -17,8 +17,8 @@ def train(evaluator, optimizer, p_id):
     board = chess.Board()
     while len(evals) < 20:
         start_time = time.time()
-        with torch.no_grad():
-            move, eval = evaluator.make_best_move(board, 2)
+        # with torch.no_grad():
+        move, eval = evaluator.make_best_move(board, 2)
         if move is None:
             evals.extend(evaluator.training_evals)
             results.extend(evaluator.training_results)
