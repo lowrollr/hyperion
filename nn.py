d42ecbd65c8272601dbfc32d8faee87dab64e892
hirollr <marshingjay@gmail.com>
2022-09-16 12:28:45 -0700

    maybe mini is the problem somehow???

--- nn.py
+++ nn.py
@@ -50,42 +50,24 @@ class HyperionDNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv3d(1, 256, (1,3,3), bias=False)
+        self.conv2 = nn.Conv3d(256, 512, (1,3,3), bias=False)
+        self.conv3 = nn.Conv3d(512, 512, (1,4,4), bias=False)
         self.bn1 = nn.BatchNorm3d(256)
         self.bn2 = nn.BatchNorm3d(512)
         self.bn3 = nn.BatchNorm3d(512)
-        self.conv2 = nn.Conv3d(256, 512, (1,3,3), bias=False)
-        self.conv3 = nn.Conv3d(512, 512, (1,4,4), bias=False)
         self.flatten = nn.Flatten(1,4)
-        self.flatten2 = nn.Flatten(1,4)
         self.lin1 = nn.Linear(9728, 9728)
         self.lin2 = nn.Linear(9728, 1)
-        self.lin3 = nn.Linear(155648, 1)
         self.tanh = nn.Tanh()
-        self.tanh2 = nn.Tanh()
 
     def forward(self, x, **kwargs):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = nn.functional.relu(x)
-        
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = nn.functional.relu(x)
-        if kwargs.get('mini'):
-            x = self.flatten2(x)
-            x = self.lin3(x)
-            x = self.tanh2(x)
-        else:
-            x = self.conv3(x)
-            x = self.bn3(x)
-            x = nn.functional.relu(x)
-            x = self.flatten(x)
-            x = self.lin1(x)
-            x = nn.functional.relu(x)
-            x = self.lin2(x)
-            x = nn.functional.relu(x)
-            x = self.tanh(x)
-        return x
+        x = nn.functional.relu(self.bn1(self.conv1(x)))
+        x = nn.functional.relu(self.bn2(self.conv2(x)))
+        x = nn.functional.relu(self.bn3(self.conv3(x)))
+        x = self.flatten(x)
+        x = nn.functional.relu(self.lin1(x))
+        x = nn.functional.relu(self.lin2(x))
+        return self.tanh(x)
     
     
 
