1d6580b31fec1b7a09454092b12920ee2803032f
hirollr <marshingjay@gmail.com>
2022-09-21 12:21:55 -0700

    make black values terms properly

--- evaluation.py
+++ evaluation.py
@@ -69,8 +69,9 @@ class MCST_Evaluator:
     def explore(self, board: chess.Board, ucb_scores) -> Tuple[float, chess.Move]:
         term_state = self.terminal_state(board)
         if term_state is not None:
-            
-            ucb_scores['t'] = float('inf') * term_state if term_state else term_state
+            term_state = term_state if board.turn else -term_state
+            result = float('inf') * term_state if term_state else term_state
+            ucb_scores['t'] = result
             ucb_scores['n'] = 1
             ucb_scores['c'] = {}
             return (term_state, None)
