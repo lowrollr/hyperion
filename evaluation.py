78d662bec2b1ae44ef186e5fb6de425cffb4b1a3
hirollr <marshingjay@gmail.com>
2022-09-21 12:32:23 -0700

    make black values terms properly

--- evaluation.py
+++ evaluation.py
@@ -165,7 +165,9 @@ class MCST_Evaluator:
         
         s, _, m = self.choose_expansion(board, self.ucb_scores, exploring=False, allow_null=False)
         self.training_boards.append(convert_to_nn_state(board))
-        print(self.ucb_scores)
+        for c, d in self.ucb_scores['c'].values():
+            if d:
+                print(c, d['t'] / d['n'])
         if m:
             self.walk_tree(m.uci())
             board.push(m)
