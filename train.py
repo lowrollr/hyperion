e77e6473774675030e4b70b9f963cb87d939a590
hirollr <marshingjay@gmail.com>
2022-09-21 17:29:03 -0700

    idk

--- train.py
+++ train.py
@@ -65,10 +65,11 @@ def mp_train(devices, epoch_games, depth, num_procs, num_epochs):
                 l_model = t_model
                 if i != 0:
                     l_model = deepcopy(t_model)
+                
                 args.append((l_model, model, device, optimizer, p_id, epoch_games, depth, num_epochs))
                 p_id += 1
+            del t_model
         results = pool.starmap(self_play, args)
-        del args
         loss, moves, times = zip(*results)
         avg_loss, avg_moves, avg_time = np.mean(loss), np.mean(moves), np.mean(times)
     # save the model
